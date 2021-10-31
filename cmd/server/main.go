ef4edc7ceab4b2f27ef191ca6460caa6d9956d58
Jorge Luis Jimenez <xadrijo@gmail.com>
2021-10-31 14:11:15 -0300

    Update main
    Update main

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,9 +1,21 @@
 package main
 
-import "log"
+import (
+	"log"
+
+	"github.com/xadrijo/grpc-vidflex/internal/db"
+	"github.com/xadrijo/grpc-vidflex/internal/product"
+)
 
 func Run() error {
 	// Initialize and start our gRPC server
+	Storage, err := db.New()
+	if err != nil {
+		return err
+	}
+
+	_ = product.New(Storage)
+
 	return nil
 }
 
